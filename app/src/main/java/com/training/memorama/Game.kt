021ccae4ef64a80b4b98a1b2e56dbc4cdbc2beae
denis.rodriguez <denis.rodriguez@et.bo>
2022-06-08 16:55:47 -0400

    Debug session 1

--- app/src/main/java/com/training/memorama/Game.kt
+++ app/src/main/java/com/training/memorama/Game.kt
@@ -1,9 +1,8 @@
 package com.training.memorama
 
-import android.R
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Toast
@@ -15,115 +14,118 @@ import java.util.*
 class Game : AppCompatActivity() {
 
     private lateinit var binding : ActivityGameBinding
-    var fondo:Int = 0
-    var arrayBarajado: ArrayList<Int>? = null
-    lateinit var imagenes:Array<Int>
-    var acierto:Int = 0
-    var puntuacion:Int = 0
-    val botonera = arrayOfNulls<ImageButton>(6)
+    var backCard:Int = 0
+    var cardShuffle: ArrayList<Int>? = null
+    lateinit var imgsList:Array<Int>
+    var _checks:Int = 0
+    var score:Int = 0
+    val btnPanel = arrayOfNulls<ImageButton>(6)
     var pause:Boolean = false
-    var primero:ImageButton? = null
-    var primeroImg:Int = 0
-    var segundoImg:Int = 0
+    var _baseTurn:ImageButton? = null
+    var _matchTurn:ImageButton? = null
+    var _baseTurnImg:Int = 0
+    var _matchTurnImg:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        loadImgs()
+        loadImages()
         botonesMenu()
-        iniciar()
+        loadConfig()
     }
-    fun loadImgs(){
-        imagenes = arrayOf<Int>(
-            com.training.memorama.R.drawable.f0,
-            com.training.memorama.R.drawable.f1,
-            com.training.memorama.R.drawable.f2,
+    private fun loadImages(){
+        this.imgsList = arrayOf<Int>(
+            R.drawable.f0,
+            R.drawable.f1,
+            R.drawable.f2,
         )
-        fondo = com.training.memorama.R.drawable.fondo
+        backCard = R.drawable.fondo
     }
-    fun barajar(longitud: Int): ArrayList<Int> {
-        val resultadoA: ArrayList<Int> = ArrayList<Int>()
-        for (i in 0 until longitud) resultadoA.add(i % longitud / 2)
-        Collections.shuffle(resultadoA)
-        return resultadoA
+    private fun loadConfig() {
+        cardShuffle = shuffleArrayCards(imgsList.size * 2)
+//        for (i in 0 until btnPanel.size) {
+//            btnPanel[i]!!.scaleType = ImageView.ScaleType.CENTER_CROP
+//            btnPanel[i]!!.setImageResource(imgsList[cardShuffle!![i]])
+//        }
+        Thread.sleep(1000)
+        for (i in 0 until btnPanel.size) {
+            btnPanel[i]!!.scaleType = ImageView.ScaleType.CENTER_CROP
+            btnPanel[i]!!.setImageResource(backCard)
+        }
+        for (i in cardShuffle!!.indices) {
+            btnPanel[i]!!.isEnabled = true
+            btnPanel[i]!!.setOnClickListener {
+                if (!pause) {
+                    evaluate(i, btnPanel[i]!!)
+                }
+            }
+        }
+        _checks = 0
+        score = 0
+        binding.tvScore.text = "$score"
     }
-    fun cargarBotones() {
-        botonera[0] = binding.ib11
-        botonera[1] = binding.ib12
-        botonera[2] = binding.ib13
-        botonera[3] = binding.ib21
-        botonera[4] = binding.ib22
-        botonera[5] = binding.ib23
-        binding.tvScore.text = "Puntuación: $puntuacion"
-    }
-    fun comprobar(i: Int, imgb: ImageButton) {
-        if (primero == null) {
-            primero = imgb
-            primero!!.setScaleType(
-                ImageView.ScaleType.CENTER_CROP
-            )
-            primero!!.setImageResource(imagenes[arrayBarajado!!.get(i)])
-            primero!!.setEnabled(false)
-            primeroImg = arrayBarajado!!.get(i)
+    private fun evaluate(i: Int, btnTap: ImageButton) {
+        if (_baseTurn == null) {
+            _baseTurn = btnTap
+            _baseTurn!!.scaleType = ImageView.ScaleType.CENTER_CROP
+            _baseTurn!!.setImageResource(imgsList[cardShuffle!!.get(i)])
+            _baseTurn!!.isEnabled = false
+            _baseTurnImg = cardShuffle!!.get(i)
         } else {
             pause = true
-            imgb.scaleType = ImageView.ScaleType.CENTER_CROP
-            imgb.setImageResource(imagenes[arrayBarajado!!.get(i)])
-            imgb.isEnabled = false
-            segundoImg = arrayBarajado!!.get(i)
-            if (primeroImg === segundoImg) {
-                primero = null
+            _matchTurn = btnTap
+            _matchTurn!!.scaleType = ImageView.ScaleType.CENTER_CROP
+            _matchTurn!!.setImageResource(imgsList[cardShuffle!!.get(i)])
+            _matchTurn!!.isEnabled = false
+            _matchTurnImg = cardShuffle!!.get(i)
+            if (_baseTurnImg === _matchTurnImg) {
+                _baseTurn = null
                 pause = false
-                acierto++
-                puntuacion++
-                binding.tvScore.text = "Puntuación: $puntuacion"
-                if (acierto === 3) {
+                _checks++
+                score++
+                binding.tvScore.text = "$score"
+                if (_checks === 3) {
                     val toast =
                         Toast.makeText(applicationContext, "Has ganado!!", Toast.LENGTH_LONG)
                     toast.show()
                 }
             } else {
-                Thread.sleep(2000)
-                primero!!.setScaleType(ImageView.ScaleType.CENTER_CROP)
-                primero!!.setImageResource(com.training.memorama.R.drawable.fondo)
-                imgb.scaleType = ImageView.ScaleType.CENTER_CROP
-                imgb.setImageResource(com.training.memorama.R.drawable.fondo)
-                primero!!.setEnabled(true)
-                imgb.isEnabled = true
-                primero = null
+                Thread.sleep(500)
                 pause = false
-                if (puntuacion > 0) {
-                    puntuacion--
-                    binding.tvScore.text = "Puntuación: $puntuacion"
+                _baseTurn!!.scaleType = ImageView.ScaleType.CENTER_CROP
+                _baseTurn!!.setImageResource(R.drawable.fondo)
+                _baseTurn!!.isEnabled = true
+                _baseTurn = null
+                btnTap.scaleType = ImageView.ScaleType.CENTER_CROP
+                btnTap.setImageResource(R.drawable.fondo)
+                btnTap.isEnabled = true
+                if (score > 0) {
+                    score--
+                    binding.tvScore.text = "$score"
                 }
-
             }
         }
     }
-    fun iniciar() {
-        arrayBarajado = barajar(imagenes.size * 2)
-        cargarBotones()
-        for (i in 0 until botonera.size) {
-            botonera[i]!!.scaleType = ImageView.ScaleType.CENTER_CROP
-            botonera[i]!!.setImageResource(imagenes[arrayBarajado!![i]])
-        }
-        Thread.sleep(1000)
-        for (i in 0 until botonera.size) {
-            botonera[i]!!.scaleType = ImageView.ScaleType.CENTER_CROP
-            botonera[i]!!.setImageResource(fondo)
-        }
-        for (i in arrayBarajado!!.indices) {
-            botonera[i]!!.isEnabled = true
-            botonera[i]!!.setOnClickListener { if (!pause) comprobar(i, botonera[i]!!) }
-        }
-        acierto = 0
-        puntuacion = 0
-        binding.tvScore.text = "Puntuación: $puntuacion"
-    }
-    fun botonesMenu(){
-
-        binding.btnReset.setOnClickListener(View.OnClickListener { iniciar() })
+    private fun botonesMenu(){
+        binding.btnReset.setOnClickListener(View.OnClickListener { loadConfig() })
         binding.btnOut.setOnClickListener(View.OnClickListener { finish() })
+    }
+    private fun shuffleArrayCards(longitud: Int): ArrayList<Int> {
+        val res: ArrayList<Int> = ArrayList<Int>()
+        for (i in 0 until longitud)
+            res.add(i % longitud / 2)
+        res.shuffle()
+        loadCards()
+        return res
+    }
+    private fun loadCards() {
+        btnPanel[0] = binding.ib11
+        btnPanel[1] = binding.ib12
+        btnPanel[2] = binding.ib13
+        btnPanel[3] = binding.ib21
+        btnPanel[4] = binding.ib22
+        btnPanel[5] = binding.ib23
+        binding.tvScore.text = "$score"
     }
 }
